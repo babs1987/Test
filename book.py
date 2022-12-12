f8189ebeeae3d9f34777d5ec5de11b35e4eecf34
babs1987 <90322556+babs1987@users.noreply.github.com>
2022-12-12 13:57:50 +0700

    optimize_getter_setter
    @property
    @year.setter

--- book.py
+++ book.py
@@ -2,20 +2,24 @@ class Book:
     def __init__(self, title, fio, year, genre, pages):
         self.title = title
         self.fio = fio
-        self.year = year
+        self._year = year
         self.genre = genre
         self.pages = pages
 
     def __str__(self):
         return f"назв: {self.title} фио: {self.fio} год: {self.year} Жанр: {self.genre} кол.стр: {self.pages}"
 
-    def get_year(self):
-        return self.year
+    @property
+    def year(self):
+        return self._year
 
-    def set_year(self, year):
-        self.year = year
+    @year.setter
+    def year(self, new_year):
+        self._year = new_year
 
 
 b1 = Book("some", "vasia pupkin", 1666, "genre", 888)
 print(b1)
-print(b1.get_year())
+print(b1.year)
+b1.year=1999
+print(b1)
